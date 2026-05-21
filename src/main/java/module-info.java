6089315fcc4a08de5ec6a492b5c273417ed5f80b
prernagrg <grgprena2023@gmail.com>
2026-05-21 23:14:43 +0545

    Initialized Intellij JavaFX project

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module com.possystem.sajilopos {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+
+
+    opens com.possystem.sajilopos to javafx.fxml;
+    exports com.possystem.sajilopos;
+}
